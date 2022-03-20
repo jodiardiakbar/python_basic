58cacef546f07130c4ae57da44293596a92c8129
Jodiardi Akbar <jodiardi.akbar@gmail.com>
2022-03-19 21:24:05 -0400

    Add files via upload

---
+++ selection_sort_demo.py
@@ -0,0 +1,14 @@
+def selection_sort(arr):
+    marker = 0
+    while marker < len(arr):
+        print(f"marker: {arr[marker]}")
+        print(f"selection sort status : {arr}")
+        start = marker + 1
+        while start < len(arr):
+            if arr[marker] > arr[start]:
+                arr[marker], arr[start] = arr[start], arr[marker]
+            start += 1
+        marker += 1
+
+l = [6, 8, 1, 4, 10, 7, 8, 9, 3, 2, 5]
+selection_sort(l)
